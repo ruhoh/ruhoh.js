4b0be13b315894b4c527a2fb1210af7c7e615e27
Jade Dominguez <plusjade@gmail.com>
2012-02-26 20:52:49 -0800

    Move router logic into Router

--- _client/require-js/app.js
+++ _client/require-js/app.js
@@ -30,6 +30,8 @@ define([
 
   var App = { 
     
+    router : new Router,
+    
     // Public: Start the application relative to the site_source.
     // The web-server is responsible for passing site_source in the Header.
     // Once the site_source folder is known we can load _config.yml and start the app.
@@ -42,47 +44,16 @@ define([
         that.config = new Config({'site_source' : '/' + jqxhr.getResponseHeader('x-ruhoh-site-source-folder') });
         return that.config.generate();
       }).done(function(){
-        that.preview = new Preview(null, that.config);
-        that.Router = new Router;
-        that.initRouting();
+        
+        that.preview = that.router.preview = new Preview(null, that.config);
+        that.router.start();
+        
       }).fail(function(jqxhr){
         Log.loadError(this, jqxhr)
       });
-    },
-    
-    // Public: Setup Routing.
-    // Preview rendering is handled by the Router.
-    // Returns: Nothing
-    initRouting : function(){
-      var that = this;
-      
-      this.Router.bind("route:home", function(){
-        that.preview.page.clear({silent : true})
-        that.preview.page.set('id', 'index.md')
-      })
-      
-      this.Router.bind("route:page", function(page){
-        var id = page.split('?id=')[1] || page;
-        
-        that.preview.page.clear({silent : true})
-        that.preview.page.set("id", id);
-      })
-      
-      // Hand off all link events to the Router.
-      $("body").find('a').live("click", function(e){
-        that.Router.navigate($(this).attr("href"), {trigger: true});
-        e.preventDefault();
-        return false;
-      });
-      
-      // Start Router.
-      Backbone.history.start({
-        pushState: true, 
-        root: (this.config.get('basePath') || '/')
-      });
     }
-
-  };
+    
+  }
   
   return App;
 });
--- _client/require-js/router.js
+++ _client/require-js/router.js
@@ -10,6 +10,41 @@ define([
       "" : "home",
       "index.html" : "home",
       "*page": "page"
+    },
+
+    // Route.navigate() events trigger these route bindings which 
+    // set the page id based on the URL.
+    // The page change:id event fires, 
+    // triggering preview.generate(): see preview model for bindings.
+    initialize : function(){
+      var that = this;
+      
+      this.bind("route:home", function(){
+        this.preview.page.clear({silent : true})
+        this.preview.page.set('id', 'index.md')
+      }, this)
+      
+      this.bind("route:page", function(page){
+        this.preview.page.clear({silent : true})
+        this.preview.page.set( 'id', (page.split('?id=')[1] || page) );
+      }, this)
+      
+      
+      // Hand off all link events to the Router.
+      $("body").find('a').live("click", function(e){
+        that.navigate($(this).attr("href"), {trigger: true});
+        e.preventDefault();
+        return false;
+      });
+    },
+    
+    // Public: Start Router.
+    // Returns: Nothing
+    start : function(){
+      Backbone.history.start({
+        pushState: true, 
+        root: (this.preview.config.get('basePath') || '/')
+      });
     }
 
   });
